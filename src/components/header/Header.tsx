3a13ed5d646df868e828cab481ca8e929f23f230
Kento Ichiki MacBook <92149111+MegaichiDimir@users.noreply.github.com>
2023-05-09 16:54:59 +0900

    add `blog` link in HeaderNav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
 	// height 56px (margin is not included)npm
 	return (
 		<header id='header'>
-			<div className="fixed z-10 px-2 py-4 w-full lg:mb-4 drop-shadow-md text-slate-800 backdrop-blur-md bg-white/30 border border-b-white/40">
+			<div className="fixed z-50 px-2 py-4 w-full lg:mb-4 drop-shadow-md text-slate-800 backdrop-blur-md bg-white/90">
 				<div className="container flex flex-row items-center justify-between mx-auto">
-					<h1 className="mr-10 text-slate-500"><Link href="/">Megaichi&#39;s site</Link></h1>
+					<h1 className="mr-10"><Link href="/">Megaichi&#39;s site</Link></h1>
 					<div className="flex flex-row items-center justify-between">
 						<HeaderNav />
 						{/* <div className="flex flex-row items-center gap-5 text-xl text-slate-400">
--- src/components/header/HeaderNav.tsx
+++ src/components/header/HeaderNav.tsx
@@ -9,11 +9,13 @@ const HeaderNav = () => {
 	const aboutMeRef = useRef<HTMLDivElement>(null);
 	const skillsRef = useRef<HTMLDivElement>(null);
 	const worksRef = useRef<HTMLDivElement>(null);
+	const blogRef = useRef<HTMLDivElement>(null);
 	const contactRef = useRef<HTMLDivElement>(null);
 
 	const aboutMeProps = useGetElementProperty<HTMLDivElement>(aboutMeRef).getElementProperty;
 	const skillsProps = useGetElementProperty<HTMLDivElement>(skillsRef).getElementProperty;
 	const worksProps = useGetElementProperty<HTMLDivElement>(worksRef).getElementProperty;
+	const blogProps = useGetElementProperty<HTMLDivElement>(blogRef).getElementProperty;
 	const contactProps = useGetElementProperty<HTMLDivElement>(contactRef).getElementProperty;
 
 	const defaultStyle = {
@@ -22,6 +24,7 @@ const HeaderNav = () => {
 		translate: `${aboutMeProps('left')}px ${aboutMeProps('top')}px`,
 		transition: 'all 0.15s ease-in-out',
 		opacity: 0, 
+		backgroundColor: 'rgb(226, 232, 240)',
 	}
 	const [focusStyle, setFocusStyle] = useState(defaultStyle as {});
 	const { intersection } = useContext(IntersectionContext);
@@ -34,6 +37,7 @@ const HeaderNav = () => {
 				translate: `${aboutMeProps('left')}px ${aboutMeProps('top')}px`,
 				transition: 'all 0.15s ease-in-out',
 				opacity: 0, 
+				backgroundColor: 'rgb(226, 232, 240)',
 			})
 		} else if (intersection === 'about-me') {
 			const style = {
@@ -41,6 +45,7 @@ const HeaderNav = () => {
 				height: aboutMeProps('height'),
 				translate: `${aboutMeProps('left')}px ${aboutMeProps('top')}px`,
 				transition: 'all 0.15s ease-in-out',
+				backgroundColor: 'rgb(226, 232, 240)',
 			}
 			setFocusStyle(style)
 		} else if (intersection === 'skills') {
@@ -49,6 +54,7 @@ const HeaderNav = () => {
 				height: skillsProps('height'),
 				translate: `${skillsProps('left')}px ${skillsProps('top')}px`,
 				transition: 'all 0.15s ease-in-out',
+				backgroundColor: 'rgb(236, 252, 203)',
 			}
 			setFocusStyle(style)
 		} else if (intersection === 'works') {
@@ -57,6 +63,16 @@ const HeaderNav = () => {
 				height: worksProps('height'),
 				translate: `${worksProps('left')}px ${worksProps('top')}px`,
 				transition: 'all 0.15s ease-in-out',
+				backgroundColor: 'rgb(224, 242, 254)',
+			}
+			setFocusStyle(style)
+		} else if (intersection === 'blog') {
+			const style = {
+				width: blogProps('width'),
+				height: blogProps('height'),
+				translate: `${blogProps('left')}px ${blogProps('top')}px`,
+				transition: 'all 0.15s ease-in-out',
+				backgroundColor: 'rgb(254, 249, 195)',
 			}
 			setFocusStyle(style)
 		} else if (intersection === 'contact') {
@@ -65,22 +81,25 @@ const HeaderNav = () => {
 				height: contactProps('height'),
 				translate: `${contactProps('left')}px ${contactProps('top')}px`,
 				transition: 'all 0.15s ease-in-out',
+				backgroundColor: 'rgb(226, 232, 240)',
 			}
 			setFocusStyle(style)
 		}
-	}, [intersection, aboutMeProps, contactProps, skillsProps, worksProps])
+
+	}, [intersection, aboutMeProps, contactProps, skillsProps, worksProps, blogProps])
 
 
 	return (
 		<>
 			<div className="flex">
 				<div>
-					<div id='focus' className="fixed top-0 left-0 z-20 rounded bg-slate-700/10" style={focusStyle}></div>
-					<div className="relative flex flex-row items-center justify-center gap-8 z-30 text-slate-600">
+					<div id='focus' className="fixed top-0 left-0 z-60 rounded" style={focusStyle}></div>
+					<div className="relative flex flex-row items-center justify-center gap-8 z-70 text-slate-800">
 						<div ref={aboutMeRef} className='px-2 rounded hover:cursor-pointer'><Scroll to='about-me' smooth={true} duration={600} offset={-56}>About Me</Scroll></div>
 						<div ref={skillsRef} className='px-2 rounded hover:cursor-pointer'><Scroll to='skills' smooth={true} duration={600} offset={-56}>Skills</Scroll></div>
 						<div ref={worksRef} className='px-2 rounded hover:cursor-pointer'><Scroll to='works' smooth={true} duration={600} offset={-56}>Works</Scroll></div>
-						<div ref={contactRef} className='px-2 rounded hover:cursor-pointer'><Scroll to='contact' smooth={true} duration={600} offset={-56}>Works</Scroll></div>
+						<div ref={blogRef} className='px-2 rounded hover:cursor-pointer'><Scroll to='blog' smooth={true} duration={600} offset={-56}>Blog</Scroll></div>
+						<div ref={contactRef} className='px-2 rounded hover:cursor-pointer'><Scroll to='contact' smooth={true} duration={600} offset={-56}>Contact</Scroll></div>
 					</div>
 				</div>
 			</div>
