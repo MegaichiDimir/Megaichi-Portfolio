d66b8d7ac2db9a9fd335335b01ff2b0ad857b4b8
Kento Ichiki MacBook <92149111+MegaichiDimir@users.noreply.github.com>
2023-05-19 16:56:26 +0900

    Remove unused comment out

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
-// import { SiTwitter, SiGithub, SiZenn } from 'react-icons/si';
+
 import HeaderNav from './HeaderNav';
 
 
 const Header = () => {
-	// height 56px (margin is not included)npm
+	// height 56px (margin is not included)
 	return (
 		<header id='header'>
 			<div className="fixed top-0 z-50 px-2 py-4 w-full lg:mb-4 drop-shadow-md text-slate-800 backdrop-blur-md bg-white/90">
@@ -12,11 +12,6 @@ const Header = () => {
 					<h1 className="mr-10"><Link href="/">Megaichi&#39;s site</Link></h1>
 					<div className="flex flex-row items-center justify-between">
 						<HeaderNav />
-						{/* <div className="flex flex-row items-center gap-5 text-xl text-slate-400">
-							<a href="https://twitter.com/Megaichi_Kenty" target="_blank" className="hover:text-slate-600 transition-colors"><SiTwitter /></a>
-							<a href="https://github.com/MegaichiDimir" target="_blank" className="hover:text-slate-600 transition-colors"><SiGithub /></a>
-							<a href="https://zenn.dev/megaichi" target="_blank" className="hover:text-slate-600 transition-colors"><SiZenn /></a>
-						</div> */}
 					</div> 
 				</div>
 			</div>
