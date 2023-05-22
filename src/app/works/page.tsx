aad6d1b38650d2d238e3c64a44990e882aa68601
Kento Ichiki MacBook <92149111+MegaichiDimir@users.noreply.github.com>
2023-05-22 16:51:54 +0900

    Fix `works` page: Add margin at the top

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,6 +1,5 @@
 import PostList from "@/components/works/PostList";
 
-
 export const metadata = {
     title: "Works",
 }
@@ -10,7 +9,7 @@ export default function StaticPage() {
 
     return (
         <>
-            <div className="relative bottom-14 min-h-screen bg-sky-100">
+            <div className="relative bottom-14 mt-14 min-h-screen bg-sky-100">
                 <div className="pt-12">
                     {/* @ts-expect-error Async Server Component */}
                     <PostList />
